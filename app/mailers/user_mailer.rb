1c25fb0627d670e5077439044b6a322e7b7b9e9a
Henry Liu <henry@liuhenry.com>
2013-09-06 13:23:20 -0700

    Rename Settings to Site

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,9 +2,9 @@ class UserMailer < ActionMailer::Base
   default from: 'team@crowdhoster.com'
 
   def payment_confirmation(payment, campaign)
-    @settings = Settings.find_by_id(1)
+    @site = Site.find_by_id(1)
     @payment = payment
     @campaign = campaign
-    mail from: "#{@settings.site_name} <#{@settings.reply_to_email}>", reply_to: @settings.reply_to_email, to: @payment.email, subject: "Your confirmation for #{@campaign.name}"
+    mail from: "#{@site.site_name} <#{@site.reply_to_email}>", reply_to: @site.reply_to_email, to: @payment.email, subject: "Your confirmation for #{@campaign.name}"
   end
 end
